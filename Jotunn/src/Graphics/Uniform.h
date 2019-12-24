1883e90359f6a0efd05ee005b72866f88656f82b
thuney <aaronthune@gmail.com>
2019-12-23 22:28:20 -0600

    Small commit toward uniform polymorphism

--- Jotunn/src/Graphics/Uniform.h
+++ Jotunn/src/Graphics/Uniform.h
@@ -19,16 +19,4 @@ namespace Jotunn
 
 	};
 
-	class IntUniform : Uniform
-	{
-		public:
-
-			IntUniform(const std::string& name, const int value) : Uniform(name)
-			{
-
-			}
-
-		private:
-
-	};
 }
---
+++ Jotunn/src/Platform/Graphics/OpenGLUniform.h
@@ -0,0 +1,18 @@
+#include "Graphics/Uniform.h"
+
+namespace Jotunn
+{
+	class IntUniform : Uniform
+	{
+
+		public:
+
+			IntUniform(const std::string& name, const int value) : Uniform(name)
+			{
+
+			}
+
+		private:
+
+	};
+}
